d10a36fe31f2a87b9900ecedf69ac2b6fc20d5f7
ksmxxxxxx <kankichi69@gmail.com>
2021-03-31 12:49:44 +0900

    :rocket: Create todo app

---
+++ index.js
@@ -0,0 +1,48 @@
+const app = Vue.createApp({
+  data () {
+    return {
+      todos: [],
+      newTodo: null
+    }
+  },
+  mounted () {
+    if (localStorage.getItem('todos')) {
+      try {
+        this.todos = JSON.parse(localStorage.getItem('todos'))
+      } catch (e) {
+        localStorage.removeItem('todos')
+      }
+    }
+  },
+  methods: {
+    addTodoItem () {
+      if (!this.newTodo) return
+      this.todos.push(
+        {
+          item: this.newTodo,
+          edit: false
+        }
+      )
+      this.newTodo = ''
+      this.saveTodoItem()
+    },
+    enableEdit (index) {
+      this.todos[index].edit = true
+      this.saveTodoItem()
+    },
+    closeEdit (index) {
+      this.todos[index].edit = false
+      this.saveTodoItem()
+    },
+    removeTodoItem (index) {
+      this.todos.splice(index, 1)
+      this.saveTodoItem()
+    },
+    saveTodoItem () {
+      let parsed = JSON.stringify(this.todos)
+      localStorage.setItem('todos', parsed)
+    }
+  }
+})
+
+app.mount('#app')
